04c88a663bd7647a4bd2097f8c0b2d2137971178
justaliang <justaliang@protonmail.com>
2023-05-13 19:06:46 +0800

    update feeder script

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ async fn main() -> Result<()> {
     let coin_type = dotenv::var("COIN_TYPE")?;
     let admin_cap_id = dotenv::var("ADMIN_CAP_ID")?;
     let oracle_id = dotenv::var("ORACLE_ID")?;
+    let clock_id = "0x0000000000000000000000000000000000000000000000000000000000000006";
 
     println!("Package ID: {}", &package_id);
     println!("Coin Type: {}", &coin_type);
@@ -39,6 +40,7 @@ async fn main() -> Result<()> {
     let coin_type = TypeTag::from_str(&coin_type)?;
     let admin_cap_id = ObjectID::from_hex_literal(&admin_cap_id)?;
     let oracle_id = ObjectID::from_hex_literal(&oracle_id)?;
+    let clock_id = ObjectID::from_hex_literal(clock_id)?;
 
     let mut interval = time::interval(time::Duration::from_secs(10));
     let market: Market = Binance::new(None, None);
@@ -58,6 +60,7 @@ async fn main() -> Result<()> {
                     vec![
                         SuiJsonValue::from_object_id(admin_cap_id),
                         SuiJsonValue::from_object_id(oracle_id),
+                        SuiJsonValue::from_object_id(clock_id),
                         SuiJsonValue::new(price_u64.into())?,
                     ],
                     &mut wallet
