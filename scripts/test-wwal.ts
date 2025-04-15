ef428c1a1bc11c8f07a6b2288ba5bbf3ea397497
paulllwu <paul@bucketprotocol.io>
2025-04-15 14:15:39 +0800

    wwal rule script done

--- scripts/test-wwal.ts
+++ scripts/test-wwal.ts
@@ -1,6 +1,8 @@
 import { BucketClient } from "bucket-protocol-sdk";
 import { Transaction } from "@mysten/sui/transactions";
 import { signer } from "./config";
+import { BLIZZARD_STAKING_OBJECT_ID, BUKCET_ORACLE_OBJECT_ID, WALRUS_SYSTEM_OBJECT_ID, WWAL_RULE_PKG_ID } from "./constant";
+
 
 const main = async () => {
   const client = new BucketClient();
@@ -8,7 +10,19 @@ const main = async () => {
 
   client.updateSupraOracle(tx, "WAL");
 
- // TODO: update wwal price
+  tx.moveCall(
+    {
+        package: WWAL_RULE_PKG_ID,
+        module: "wwal_rule",
+        function: "update_price",
+        arguments: [
+            tx.object(BUKCET_ORACLE_OBJECT_ID),
+            tx.object(BLIZZARD_STAKING_OBJECT_ID),
+            tx.object(WALRUS_SYSTEM_OBJECT_ID),
+            tx.object("0x6"),
+        ],
+    }
+  );
 
   const res = await client.getSuiClient().signAndExecuteTransaction({
     transaction: tx,
